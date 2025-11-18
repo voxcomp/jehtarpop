d1c9000dc579cadece2b0a9b14341e3674bfbdfd
Shift <shift@laravelshift.com>
2025-11-18 15:36:13 +0000

    Shift cleanup

--- config/app.php
+++ config/app.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
 
 return [
 
     'timezone' => 'America/New_York',
-
 
     'aliases' => Facade::defaultAliases()->merge([
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
