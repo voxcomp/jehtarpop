3ae28aa2b60fcf88d38c5b8aa790eb90583d20ec
Shift <shift@laravelshift.com>
2025-11-18 15:36:07 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,32 @@
 
 namespace App\Providers;
 
+use App\Events\RegistrationProcessed;
+use App\Listeners\SendRegistrationNotification;
 use App\Models\SupportTicket;
 use App\Models\SupportTicketFile;
 use App\Models\SupportTicketNote;
 use App\Observers\SupportTicketFileObserver;
 use App\Observers\SupportTicketNoteObserver;
 use App\Observers\SupportTicketObserver;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     /**
      * Register any application services.
      */
@@ -30,5 +45,24 @@ class AppServiceProvider extends ServiceProvider
         SupportTicketNote::observe(SupportTicketNoteObserver::class);
         SupportTicketFile::observe(SupportTicketFileObserver::class);
         Schema::defaultStringLength(191);
+
+        $this->bootEvent();
+        $this->bootRoute();
+    }
+
+    public function bootEvent(): void
+    {
+
+        Event::listen(
+            RegistrationProcessed::class,
+            [SendRegistrationNotification::class, 'handle']
+        );
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
